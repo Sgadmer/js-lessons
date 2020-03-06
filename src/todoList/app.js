5d5fa43d76e6cf6e6003fbcddac04d08fca1ea32
Sgadmer <r.smim@yandex.ru>
2020-03-06 23:12:14 +0400

    Сделал сортировку по выбору

--- src/todoList/app.js
+++ src/todoList/app.js
@@ -4,7 +4,7 @@ const formElement = document
 const inputElement = document
   .querySelector('#task');
 
-const ulElement = document
+const olElement = document
   .querySelector('#tasks');
 
 function onInputChange(event) {
@@ -16,7 +16,8 @@ function onFormSubmit(event) {
   const value = inputElement.value.trim();
   if (!value) { return; }
   const liElement = createElement('li', value);
-  ulElement.appendChild(liElement);
+  liElement.setAttribute('completed',false);
+  olElement.appendChild(liElement);
   inputElement.value = '';
 }
 
@@ -33,4 +34,46 @@ inputElement
 
 formElement
   .addEventListener('submit', onFormSubmit);
+
+
+  
+//Кнопки
+
+const buttonFilter = document
+  .querySelectorAll('button[filter]');
+
+const todo = document
+  .querySelector('#todo-list');
+
+buttonFilter.forEach((elem) => {
+  elem.addEventListener('click', onFilterButtonClick);
+});
+
+function onFilterButtonClick(event) {
+  let elem = event.target;
+  let filterRule = elem.getAttribute('filter');
+  todo.setAttribute('filterMode', filterRule);
+
+  buttonFilter.forEach((button) => {
+    button.setAttribute('isActivated', false);
+  })
+  elem.setAttribute('isActivated', true);
+}
+
+
+//Переключатель статуса задач
+
+olElement
+  .addEventListener('click', olClick);
+
+function olClick(event) {
+  let target = event.target;
+  if (!target.tagName == 'li') return;
+
+  if (target.getAttribute('completed')=='false') {
+    target.setAttribute('completed', true);
+  } else { target.setAttribute('completed',false); }
+}
+
+
 
