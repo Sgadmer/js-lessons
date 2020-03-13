f915af6c5e295d3236825d16842d80e3ba999738
Sgadmer <r.smim@yandex.ru>
2020-03-13 23:35:05 +0400

    Сделал задание без промисов

--- src/listOfMovies/src/main.js
+++ src/listOfMovies/src/main.js
@@ -1,11 +1,25 @@
 let movieXHR = new XMLHttpRequest();
 
-movieXHR.open('GET', 'http://img.omdbapi.com/?apikey=[586fb65b]&', true);
+movieXHR.open('GET', 'http://www.omdbapi.com/?apikey=586fb65b&s=The Hunger Games', true);
+
+
+movieXHR.responseType = 'json';
 
 movieXHR.onload = () =>{
-    console.log(movieXHR);
+    
+
+    let movies = document.querySelector('#movies');
+let arrPoster = movieXHR.response.Search;
+
+arrPoster.forEach(element => {
+    
+    let newPoster = document.createElement('img');
+    newPoster.setAttribute('src', element.Poster);
+    movies.append(newPoster);
+
+});
+
 }
 
 movieXHR.send();
-
 
