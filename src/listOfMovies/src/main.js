dc5a44ad167cd190f3e03f6e2819e86e615d2ce6
Sgadmer <r.smim@yandex.ru>
2020-03-13 11:46:35 +0400

    Invalid API KEY

--- src/listOfMovies/src/main.js
+++ src/listOfMovies/src/main.js
@@ -1,6 +1,6 @@
 let movieXHR = new XMLHttpRequest();
 
-movieXHR.open('GET', 'http://img.omdbapi.com/?apikey=[586fb65b]&r', true);
+movieXHR.open('GET', 'http://img.omdbapi.com/?apikey=[586fb65b]&', true);
 
 movieXHR.onload = () =>{
     console.log(movieXHR);
